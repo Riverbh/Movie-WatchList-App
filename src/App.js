d259354fa9f640f886f3bf9b97ef3842579cfd29
River Hansen <riverbh01@gmail.com>
2023-04-24 09:53:47 -0600

    complteted the movie watchlist app code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 import Header from './Components/Header';
 import axios from 'axios';
 import MovieScreen from './Components/MovieScreen';
+import Watchlist from './Components/Watchlist';
+
 
 function App() {
   const [movieList, setMovieList] = useState([]);
   const [watchList, setWatchList] = useState([]);
   const [page, setPage] = useState(1);
 
-  const getData = () => {
+  const addMovie = (movie) => setWatchList([...movieList, movie])
+
+  const removeMovie = (movie) => {
+    const newState = movieList.filter((mov) => {
+      return mov !== movie
+    })
+    setMovieList(newState)
+  } 
+
+  const getData = (props) => {
     axios
       .get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`)
       .then((res) => {
@@ -22,16 +33,21 @@ function App() {
     getData();
   }, [page]);
 
+
+
   return (
     <div className="App">
       <Header />
       <main>
         <MovieScreen 
+        addMovie={addMovie}
         movieList={movieList}
         page={page}
         setPage={setPage}
         watchList={watchList}
+        removeMovie={removeMovie}
         />
+        <Watchlist list={movieList} removeMovie={removeMovie}/>
       </main>
     </div>
   );
